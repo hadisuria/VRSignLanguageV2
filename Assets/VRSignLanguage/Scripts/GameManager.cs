b9f027dc3310024dddb2df2d6eaf8d9fd2617959
Hadi <61531010+hadisuria@users.noreply.github.com>
2021-10-11 22:55:21 +0700

    Format code

--- Assets/VRSignLanguage/Scripts/GameManager.cs
+++ Assets/VRSignLanguage/Scripts/GameManager.cs
@@ -1,71 +1,72 @@
-﻿using System.Diagnostics;
-using UnityEngine;
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using System;
+using System.Diagnostics;
+using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-	[SerializeField] private VRInputHandler inputHandler;
-	[SerializeField] private BoardMenuController menuController;
+    [SerializeField] private VRInputHandler inputHandler;
+    [SerializeField] private BoardMenuController menuController;
 
-	private bool prevInputHandlerPrimaryButtonLeft = false;
-	private bool prevInputHandlerPrimaryButtonRight = false;
-	public static bool isRayActive { get; private set; }
-	[SerializeField] private VRInputModule vrInputModule;
-
- 
-
-	private void Awake()
-	{
-
-	} 
-
-	private void Update()
-	{
-		HandleRayActive();
-	}
-
-	private void HandleRayActive() {
-		bool rightPrimaryButton =  inputHandler.GetRightHandController().primaryButton;
-		bool leftPrimaryButton = inputHandler.GetLeftHandController().primaryButton;
+    private bool prevInputHandlerPrimaryButtonLeft = false;
+    private bool prevInputHandlerPrimaryButtonRight = false;
+    public static bool isRayActive { get; private set; }
+    [SerializeField] private VRInputModule vrInputModule;
 
 
-		if(vrInputModule.mainController == OVRInput.Controller.RTouch)
-		{
-			if (rightPrimaryButton)
-			{
-				if (!prevInputHandlerPrimaryButtonRight)
-				{
-					prevInputHandlerPrimaryButtonRight = true; ;
-					isRayActive = !isRayActive;
-				}
-			}
-			else
-			{
-				prevInputHandlerPrimaryButtonRight = false;
-			}
-		}
-		else if(vrInputModule.mainController == OVRInput.Controller.LTouch)
-		{
-			if (leftPrimaryButton)
-			{
-				if (!prevInputHandlerPrimaryButtonLeft)
-				{
-					prevInputHandlerPrimaryButtonLeft = true;
-					isRayActive = !isRayActive;
-				}
-			}
-			else
-			{
-				prevInputHandlerPrimaryButtonLeft = false;
+
+    private void Awake()
+    {
+
+    }
+
+    private void Update()
+    {
+        HandleRayActive();
+    }
+
+    private void HandleRayActive()
+    {
+        bool rightPrimaryButton = inputHandler.GetRightHandController().primaryButton;
+        bool leftPrimaryButton = inputHandler.GetLeftHandController().primaryButton;
 
 
-				
-			}
-		}
+        if (vrInputModule.mainController == OVRInput.Controller.RTouch)
+        {
+            if (rightPrimaryButton)
+            {
+                if (!prevInputHandlerPrimaryButtonRight)
+                {
+                    prevInputHandlerPrimaryButtonRight = true; ;
+                    isRayActive = !isRayActive;
+                }
+            }
+            else
+            {
+                prevInputHandlerPrimaryButtonRight = false;
+            }
+        }
+        else if (vrInputModule.mainController == OVRInput.Controller.LTouch)
+        {
+            if (leftPrimaryButton)
+            {
+                if (!prevInputHandlerPrimaryButtonLeft)
+                {
+                    prevInputHandlerPrimaryButtonLeft = true;
+                    isRayActive = !isRayActive;
+                }
+            }
+            else
+            {
+                prevInputHandlerPrimaryButtonLeft = false;
 
-	}
+
+
+            }
+        }
+
+    }
 
 
 }
