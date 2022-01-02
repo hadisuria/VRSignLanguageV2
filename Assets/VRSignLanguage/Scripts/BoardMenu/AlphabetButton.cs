86f360ada8e31913ed87533b0ba51572a2791bd7
Hadi <hadisuria789@gmail.com>
2022-01-02 21:14:25 +0700

    Add AlphabetButton Controller

---
+++ Assets/VRSignLanguage/Scripts/BoardMenu/AlphabetButton.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+[RequireComponent(typeof(ButtonEvent))]
+public class AlphabetButton : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI myString;
+    private ButtonEvent buttonEvent;
+    private BoardMenuController menuController;
+
+    // private void Awake()
+    // {
+    //     buttonEvent = GetComponent<ButtonEvent>();
+    //     buttonEvent.OnButtonClicked += OnWordClicked;
+    //     menuController = FindObjectOfType<BoardMenuController>();
+    // }
+
+    public void Init(String alphabet)
+    {
+        SetText(alphabet);
+    }
+
+    private void SetText(string textString)
+    {
+        myString.text = textString;
+    }
+
+    private void OnWordClicked()
+    {
+        //wordListControl.ButtonClick(myTextString);
+        // menuController.OpenMenu(BoardMenuID.SignLanguagePreview, myGuideBall);
+    }
+}
---
+++ Assets/VRSignLanguage/Scripts/BoardMenu/DictionaryAlphabetListController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DictionaryAlphabetListController : MonoBehaviour
+{
+    void Start()
+    {
+        foreach (int i in Enum.GetValues(typeof(AlphabetID)))
+        {
+
+            AlphabetButton temp = Instantiate(Resources.Load<GameObject>("Menu/AlphabetButton"), transform).GetComponent<AlphabetButton>();
+            temp.gameObject.SetActive(true);
+
+            temp.Init(((AlphabetID)i).ToString());
+
+            Debug.Log(((AlphabetID)i).ToString());
+
+            // buttons.Add(temp);
+        }
+    }
+
+}
