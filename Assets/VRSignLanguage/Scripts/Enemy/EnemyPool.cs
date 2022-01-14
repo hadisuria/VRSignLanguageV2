242ae431b679d1e337c801ae588fd863828d65db
Hadi <hadisuria789@gmail.com>
2022-01-14 22:12:31 +0700

    Prevent alphabet J and Z

--- Assets/VRSignLanguage/Scripts/Enemy/EnemyPool.cs
+++ Assets/VRSignLanguage/Scripts/Enemy/EnemyPool.cs
@@ -9,6 +9,7 @@ public class EnemyPool : MonoBehaviour
     [SerializeField] private Transform[] spawnPos = new Transform[3];
     private List<Enemy> unusedEnemies = new List<Enemy>();
     public readonly List<Enemy> usedEnemies = new List<Enemy>();
+    private int currAlphabetInt = 0;
 
     public Enemy SummonEnemy()
     {
@@ -25,7 +26,12 @@ public class EnemyPool : MonoBehaviour
         }
 
         //initialize random alphabet for enemy
-        temp.alphabet = (AlphabetID)UnityEngine.Random.Range(1, 27);
+        // looping to prevent enemy spawn with alphabet J and Z
+        do
+        {
+            currAlphabetInt = UnityEngine.Random.Range(1, 27);
+        } while (currAlphabetInt == 10 || currAlphabetInt == 27);
+        temp.alphabet = (AlphabetID)currAlphabetInt;
 
         temp.transform.position = spawnPos[UnityEngine.Random.Range(0, 3)].position;
         temp.gameObject.SetActive(true);
